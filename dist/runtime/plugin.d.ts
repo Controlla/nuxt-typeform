bd77198e716dcd94883aff08270c88ff0f5f7c90
Ivan Sotelo <isotelo@controlla.com.mx>
2022-08-11 13:31:43 -0600

    fix: remove dist

--- dist/runtime/plugin.d.ts
+++
@@ -1,3 +0,0 @@
-import '@typeform/embed/build/css/popup.css';
-declare const _default: any;
-export default _default;
--- dist/runtime/plugin.mjs
+++
@@ -1,8 +0,0 @@
-import "@typeform/embed/build/css/popup.css";
-import { createPopup } from "@typeform/embed";
-import { defineNuxtPlugin, useRuntimeConfig } from "#imports";
-export default defineNuxtPlugin((nuxtApp) => {
-  const { formId } = useRuntimeConfig().typeform;
-  const popup = createPopup(formId);
-  nuxtApp.provide("typeform", { popup });
-});
